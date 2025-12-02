f4196c3101b789aaff18ecdd626e1458adec02a5
Hao079231 <haotrinh2003.com@gmail.com>
2025-12-02 20:28:14 +0700

    fix percent in simulation

--- source/user-base-auth/src/main/java/com/base/auth/mapper/SimulationMapper.java
+++ source/user-base-auth/src/main/java/com/base/auth/mapper/SimulationMapper.java
@@ -70,7 +70,6 @@ public interface SimulationMapper {
   @Mapping(source = "videoPath", target = "videoPath")
   @Mapping(source = "avgRating", target = "avgRating")
   @Mapping(source = "participantQuantity", target = "participantQuantity")
-  @Mapping(source = "percent", target = "percent")
   @Mapping(source = "specialization", target = "specialization", qualifiedByName = "fromEntityToSpecializationAutoCompleteDto")
   @Mapping(source = "educator", target = "educator", qualifiedByName = "fromEducatorToProfileDto")
   @BeanMapping(ignoreByDefault = true)
